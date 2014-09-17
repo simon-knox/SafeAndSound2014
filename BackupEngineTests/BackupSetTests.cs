09bdeb93e130eeb7cf81e160ce20525336c25e66
simon <simon@sknoxconsulting.co.uk>
2014-09-17 13:56:01 +0100

    Adding Tests

---
+++ BackupEngineTests/BackupSetTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SKnoxConsulting.SafeAndSound.BackupEngine;
+using System.Threading;
+
+namespace SKnoxConsulting.SafeAndSound.BackupEngineTests
+{
+    [TestClass]
+    public class BackupSetTests
+    {
+        [TestMethod]
+        [Ignore]
+        public void RunBackupTest()
+        {
+            try
+            {
+                var bs = new BackupSet("TEST", @"D:\TestData", @"D:\TestData2");
+                Assert.IsTrue(bs.Status == "Ready.");
+                bs.RunBackup();
+
+                while (bs.Status != "Finished.")
+                {
+                    Thread.Sleep(100);
+                }
+                Assert.IsTrue(bs.FileCopyCount + bs.FileOverwriteCount + bs.FileSkipCount == bs.TotalFileCount, "File counts don't add up");
+                Assert.IsTrue(bs.TotalFileCountMaximum == bs.TotalFileCount, "Didn't react maximum count");
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+    }
+}
---
+++ BackupEngineTests/CopyFileActionTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SKnoxConsulting.SafeAndSound.BackupEngine.BackupActions;
+using SKnoxConsulting.SafeAndSound.BackupEngine;
+using System.Threading;
+//using SKnoxConsulting.SafeAndSound.BackupEngine.BackupActions;
+
+namespace SKnoxConsulting.SafeAndSound.BackupEngineTests
+{
+    [TestClass]
+    public class CopyFileActionTests
+    {
+        [TestMethod]
+        public void CanCreateCopyFileAction()
+        {
+            var copyFileAction = new CopyFileAction("MySource", "MyDestination");
+            Assert.IsTrue(copyFileAction.Source == "MySource", string.Format("Source is {0} instead of MySource", copyFileAction.Source));
+            Assert.IsTrue(copyFileAction.Destination == "MyDestination", string.Format("Destination is {0} instead of MyDestination", copyFileAction.Destination));
+
+
+
+
+
+        }
+
+
+
+    }
+}
