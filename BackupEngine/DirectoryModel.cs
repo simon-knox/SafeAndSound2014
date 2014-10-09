4ce889272fd45436b5df858933686eec36770ed5
simon <simon@sknoxconsulting.co.uk>
2014-10-09 17:05:13 +0100

    Test

---
+++ BackupEngine/DirectoryModel.cs
@@ -0,0 +1,53 @@
+﻿using Catel.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKnoxConsulting.SafeAndSound.BackupEngine
+{
+    public class DirectoryModel : ModelBase
+    {
+        public DirectoryModel(string path)
+        {
+            var di = new DirectoryInfo(path);
+            Name = di.Name;
+            FullPath = di.FullName;
+            SubDirectories = di.GetDirectories("*", SearchOption.TopDirectoryOnly).Select(d => new DirectoryModel(d.FullName));
+        }
+        
+        public static readonly PropertyData NameProperty = RegisterProperty("Name", typeof(string));
+
+        public string Name
+        {
+            get { return GetValue<string>(NameProperty); }
+            private set { SetValue(NameProperty, value); }
+        }
+
+        public static readonly PropertyData FullPathProperty = RegisterProperty("FullPath", typeof(string));
+
+        public string FullPath
+        {
+            get { return GetValue<string>(FullPathProperty); }
+            private set { SetValue(FullPathProperty, value); }
+        }
+
+        public static readonly PropertyData IsCheckedProperty = RegisterProperty("IsChecked", typeof(bool), true);
+
+        public bool IsChecked
+        {
+            get { return GetValue<bool>(IsCheckedProperty); }
+           set { SetValue(IsCheckedProperty, value); }
+        }
+
+        public static readonly PropertyData SubDirectoriesProperty = RegisterProperty("SubDirectories", typeof(IEnumerable<DirectoryModel>));
+
+        public IEnumerable<DirectoryModel> SubDirectories
+        {
+            get { return GetValue<IEnumerable<DirectoryModel>>(SubDirectoriesProperty); }
+            private set { SetValue(SubDirectoriesProperty, value); }
+        }
+    }
+}
